b74b52c1da28546911d6e9a4c2faad73bb1da441
fauzandwip <fauzan27dp@gmail.com>
2024-03-28 16:01:12 +0700

    feat: slicing detail post page integrate detail post integrate comments in detail post

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,6 +3,7 @@
 import PostCard from '@/components/PostCard';
 import { GOREST_URL } from '@/config/config';
 import { Post } from '@/types/post';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 
 const PostsPage = () => {
@@ -24,7 +25,11 @@ const PostsPage = () => {
 	return (
 		<div className=" w-full min-h-screen bg-gray-300 flex flex-col gap-8 p-8">
 			{posts.map((post: Post) => {
-				return <PostCard key={post.id} post={post} />;
+				return (
+					<Link key={post.id} href={`/posts/${post.id}`}>
+						<PostCard post={post} />
+					</Link>
+				);
 			})}
 		</div>
 	);
