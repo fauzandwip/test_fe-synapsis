032273df9648e0f3c130ed38e679e9a8b611fc25
fauzandwip <fauzan27dp@gmail.com>
2024-03-29 14:40:38 +0700

    feat: integration search users by name

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -6,6 +6,7 @@ import IconPlus from '@/components/icons/IconPlus';
 import { GOREST_URL } from '@/config/config';
 import { User } from '@/types/user';
 import React, { FormEvent, useEffect, useState } from 'react';
+import { setTimeout } from 'timers';
 
 const UsersPage = () => {
 	const [users, setUsers] = useState<User[]>([]);
@@ -16,12 +17,13 @@ const UsersPage = () => {
 		gender: '',
 		status: '',
 	});
+	const [search, setSearch] = useState('');
 	const [showAddForm, setShowAddForm] = useState(false);
 	const [showEditForm, setshowEditForm] = useState(false);
 
 	const fetchUsers = async () => {
 		try {
-			const response = await fetch(`${GOREST_URL}/users`, {
+			const response = await fetch(`${GOREST_URL}/users?name=${search}`, {
 				headers: {
 					Authorization: `Bearer ${process.env.NEXT_PUBLIC_GOREST_ACCESS_TOKEN}`,
 				},
@@ -35,7 +37,7 @@ const UsersPage = () => {
 
 	useEffect(() => {
 		fetchUsers();
-	}, []);
+	}, [search]);
 
 	const onChangeCurrentUser = (field: string, value: string) => {
 		setCurrentUser((prev) => ({ ...prev, [field]: value }));
@@ -107,10 +109,24 @@ const UsersPage = () => {
 
 	return (
 		<div className=" w-full min-h-screen bg-gray-300 p-8 relative">
-			<div className="p-4 flex justify-end">
+			<div className="p-4 flex justify-end items-center gap-2">
+				<div className="w-full p-2">
+					<input
+						onChange={async (e) => {
+							setTimeout(() => {
+								setSearch(e.target.value);
+							}, 600);
+						}}
+						type="text"
+						name="search"
+						id="search"
+						className="w-full px-4 py-2 rounded-lg border-2 border-black shadow-neu outline-none focus:bg-lime-500 bg-white focus:placeholder-slate-200 placeholder-slate-400"
+						placeholder="search by name"
+					/>
+				</div>
 				<button
 					onClick={toggleShowAddForm}
-					className=" w-max p-2 bg-blue-800 rounded-full hover:bg-blue-900 active:bg-blue-950 active:scale-95 transition-transform"
+					className="p-2 h-max bg-blue-800 rounded-full hover:bg-blue-900 active:bg-blue-950 active:scale-95 transition-transform"
 				>
 					<IconPlus width={'2em'} height={'2em'} className="text-slate-100" />
 				</button>
